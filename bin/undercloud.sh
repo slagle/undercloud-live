2e67da6777ceb0f9a7bc5c4c709836aa819a9713
James Slagle <jslagle@redhat.com>
2013-08-16 13:59:26 -0400

    network setup is actually run directly after configure

--- bin/undercloud.sh
+++ bin/undercloud.sh
@@ -4,6 +4,7 @@ set -eux
 
 $(dirname $0)/undercloud-install.sh
 $(dirname $0)/undercloud-configure.sh
+$(dirname $0)/undercloud-network.sh
 
 # starts all services and runs os-refresh-config (via os-collect-config
 # service)
@@ -22,6 +23,5 @@ while true; do
 done
 
 # Download images
-$(dirname $0)/undercloud-network.sh
 $(dirname $0)/undercloud-images.sh
 $(dirname $0)/undercloud-setup.sh
