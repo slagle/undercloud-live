042153c1bd59693c777df21c3dc973e74cbc0602
James Slagle <jslagle@redhat.com>
2013-08-16 14:40:11 -0400

    just running os-refresh-config is sufficient

--- bin/undercloud.sh
+++ bin/undercloud.sh
@@ -9,18 +9,7 @@ $(dirname $0)/undercloud-network.sh
 # starts all services and runs os-refresh-config (via os-collect-config
 # service)
 sudo systemctl daemon-reload
-sudo systemctl isolate multi-user.target
-
-# Wait for os-collect-config (os-refresh-config) to finish.
-while true; do
-    systemctl show os-collect-config | grep Result=success
-    rc=$?
-    if [ $rc -eq 0 ]; then
-        break
-    fi
-    echo "os-collect-config not yet done, sleeping..."
-    sleep 10
-done
+sudo os-refresh-config
 
 # Download images
 $(dirname $0)/undercloud-images.sh
