849f71d594dc42f22cdc4eca5d7a19b9fd7504be
James Slagle <jslagle@redhat.com>
2013-08-16 15:29:04 -0400

    move to setup network script

--- bin/undercloud-network.sh
+++ bin/undercloud-network.sh
@@ -2,6 +2,9 @@
 
 PUBLIC_INTERFACE=${PUBLIC_INTERFACE:-ucl0}
 
+sudo sed -i "s/bridge name='brbm'/bridge name='br-ctlplane'/" /opt/stack/tripleo-incubator/templates/brbm.xml
+/opt/stack/tripleo-incubator/scripts/setup-network
+
 sudo ip link add $PUBLIC_INTERFACE type dummy
 # init-neutron-ovs is also executed by os-refresh-config, but it's done here
 # because we can stop and make sure it's done the right thing easier to debug,
