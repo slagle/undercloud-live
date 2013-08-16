43d007ce34ca009f23a7ec538493ecc4378637bb
James Slagle <jslagle@redhat.com>
2013-08-16 13:58:15 -0400

    add script to download images

---
+++ bin/undercloud-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Eventually, this script may run diskimage-builder commands, for now it just
+# downloads images from a preconfigured location.
+
+set -eux
+
+mkdir -p /opt/stack/images
+pushd /opt/stack/images
+
+curl -L -O $DEPLOY_RAMDISK_URL
+curl -L -O $DEPLOY_INITRAMFS_URL
+curl -L -O $OVERCLOUD_COMPUTE_URL
+curl -L -O $OVERCLOUD_CONTROLE_URL
+
+# This is a fast mirror, for me anyway :-).
+curl -L -O http://mirror.cogentco.com/pub/linux/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
+
+popd
