02a84f36c685ce7b6cac30b02336798aa31ce560
James Slagle <jslagle@redhat.com>
2013-08-16 14:02:58 -0400

    run as root

--- bin/undercloud-network.sh
+++ bin/undercloud-network.sh
@@ -6,5 +6,5 @@ sudo ip link add eth1 type dummy
 # init-neutron-ovs is also executed by os-refresh-config, but it's done here
 # because we can stop and make sure it's done the right thing easier to debug,
 # etc.
-init-neutron-ovs
+sudo init-neutron-ovs
 
