8fbf92ef7b1c1f9c254138dd620a575a2e824bb2
James Slagle <jslagle@redhat.com>
2013-08-16 11:35:24 -0400

    main script should call configure script

--- bin/undercloud-configure.sh
+++ bin/undercloud-configure.sh
@@ -79,22 +79,4 @@ sudo sed -i "s/eth1/$PUBLIC_INTERFACE/g" /var/lib/heat-cfntools/cfn-init-data
 
 sudo sed -i "s/192.168.122.1/$NETWORK/g" /opt/stack/os-config-applier/templates/var/opt/undercloud-live/masquerade
 
-export UNDERCLOUD_IP=192.0.2.1
-/opt/stack/tripleo-incubator/scripts/setup-neutron 192.0.2.5 192.0.2.24 192.0.2.0/24 $UNDERCLOUD_IP ctlplane
-
-# Adds default ssh key to nova
-/opt/stack/tripleo-incubator/scripts/user-config
-
-# Baremetal setup
-/opt/stack/tripleo-incubator/scripts/create-nodes 1 1024 10 3
-# MACS must be set for setup-baremetal to work
-export MACS=$(bm_poseur get-macs)
-sudo sed -i "s/ubuntu/undercloud-live/g" /opt/stack/tripleo-incubator/scripts/register-nodes
-# $TRIPLEO_ROOT is not true to the tripleo sense, but it's where
-# setup-baremetal look for the deploy kernel and ramfs.
-TRIPLEO_ROOT=/opt/stack/images /opt/stack/tripleo-incubator/scripts/setup-baremetal 1 1024 10 undercloud
-cat /opt/stack/boot-stack/virtual-power-key.pub >> ~/.ssh/authorized_keys
-/opt/stack/tripleo-incubator/scripts/load-image /opt/stack/images/overcloud-compute.qcow2
-/opt/stack/tripleo-incubator/scripts/load-image /opt/stack/images/overcloud-control.qcow2
-
 touch /opt/stack/undercloud-live/.undercloud-init
--- bin/undercloud.sh
+++ bin/undercloud.sh
@@ -3,7 +3,7 @@
 set -eux
 
 $(dirname $0)/undercloud-install.sh
-$(dirname $0)/undercloud-init.sh
+$(dirname $0)/undercloud-configure.sh
 
 # starts all services and runs os-refresh-config (via os-collect-config
 # service)
@@ -20,3 +20,5 @@ while true; do
     echo "os-collect-config not yet done, sleeping..."
     sleep 10
 done
+
+$(dirname $0)/undercloud-setup.sh
