1a43dbd1b551dd057aee75620c4498bea32cde7c
James Slagle <jslagle@redhat.com>
2013-08-19 09:54:47 -0400

    fix bad url names

--- bin/undercloud-images.sh
+++ bin/undercloud-images.sh
@@ -8,10 +8,10 @@ set -eux
 mkdir -p /opt/stack/images
 pushd /opt/stack/images
 
-curl -L -O $DEPLOY_RAMDISK_URL
+curl -L -O $DEPLOY_KERNEL_URL
 curl -L -O $DEPLOY_INITRAMFS_URL
 curl -L -O $OVERCLOUD_COMPUTE_URL
-curl -L -O $OVERCLOUD_CONTROLE_URL
+curl -L -O $OVERCLOUD_CONTROL_URL
 
 # This is a fast mirror, for me anyway :-).
 curl -L -O http://mirror.cogentco.com/pub/linux/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
