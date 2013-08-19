1a89f8d10bb080b42b348c2da22b4fc6723acc42
James Slagle <jslagle@redhat.com>
2013-08-19 17:47:29 -0400

    This logic moved into tripleo-incubator branch

--- bin/undercloud-setup.sh
+++ bin/undercloud-setup.sh
@@ -16,7 +16,6 @@ export UNDERCLOUD_IP=192.0.2.1
 /opt/stack/tripleo-incubator/scripts/create-nodes 1 1024 10 3
 # MACS must be set for setup-baremetal to work
 export MACS=$(bm_poseur get-macs)
-sudo sed -i "s/ubuntu/undercloud-live/g" /opt/stack/tripleo-incubator/scripts/register-nodes
 # $TRIPLEO_ROOT is not true to the tripleo sense, but it's where
 # setup-baremetal look for the deploy kernel and ramfs.
 TRIPLEO_ROOT=/opt/stack/images /opt/stack/tripleo-incubator/scripts/setup-baremetal 1 1024 10 undercloud
