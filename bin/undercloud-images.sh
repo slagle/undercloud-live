8efb34233e193d7850a69dcd16f2ce83f4dbfca8
James Slagle <jslagle@redhat.com>
2013-08-20 09:35:52 -0400

    Move image commands into undercloud-images.sh so that it can be run seperately

--- bin/undercloud-images.sh
+++ bin/undercloud-images.sh
@@ -17,3 +17,11 @@ curl -L -O $OVERCLOUD_CONTROL_URL
 curl -L -O http://mirror.cogentco.com/pub/linux/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
 
 popd
+
+# MACS must be set for setup-baremetal to work
+export MACS=$(bm_poseur get-macs)
+# $TRIPLEO_ROOT is not true to the tripleo sense, but it's where
+# setup-baremetal look for the deploy kernel and ramfs.
+TRIPLEO_ROOT=/opt/stack/images /opt/stack/tripleo-incubator/scripts/setup-baremetal 1 1024 10 undercloud
+/opt/stack/tripleo-incubator/scripts/load-image /opt/stack/images/overcloud-compute.qcow2
+/opt/stack/tripleo-incubator/scripts/load-image /opt/stack/images/overcloud-control.qcow2
--- bin/undercloud-setup.sh
+++ bin/undercloud-setup.sh
@@ -12,13 +12,7 @@ export UNDERCLOUD_IP=192.0.2.1
 # Adds default ssh key to nova
 /opt/stack/tripleo-incubator/scripts/user-config
 
+cat /opt/stack/boot-stack/virtual-power-key.pub >> ~/.ssh/authorized_keys
+
 # Baremetal setup
 /opt/stack/tripleo-incubator/scripts/create-nodes 1 1024 10 3
-# MACS must be set for setup-baremetal to work
-export MACS=$(bm_poseur get-macs)
-# $TRIPLEO_ROOT is not true to the tripleo sense, but it's where
-# setup-baremetal look for the deploy kernel and ramfs.
-TRIPLEO_ROOT=/opt/stack/images /opt/stack/tripleo-incubator/scripts/setup-baremetal 1 1024 10 undercloud
-cat /opt/stack/boot-stack/virtual-power-key.pub >> ~/.ssh/authorized_keys
-/opt/stack/tripleo-incubator/scripts/load-image /opt/stack/images/overcloud-compute.qcow2
-/opt/stack/tripleo-incubator/scripts/load-image /opt/stack/images/overcloud-control.qcow2
