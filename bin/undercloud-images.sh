d19f536d5cc6c8b72f50d2d92534daf89d4ba70c
James Slagle <jslagle@redhat.com>
2013-08-20 12:01:19 -0400

    flesh out script

--- bin/undercloud-images.sh
+++ bin/undercloud-images.sh
@@ -5,18 +5,50 @@
 
 set -eux
 
-mkdir -p /opt/stack/images
-pushd /opt/stack/images
+IMAGES_DIR=/opt/stack/images
+CONTROL_IMG=$IMAGES_DIR/overcloud-control.qcow2
+COMPUTE_IMG=$IMAGES_DIR/overcloud-compute.qcow2
+BM_KERNEL=$IMAGES_DIR/deploy-ramdisk.kernel
+BM_INITRAMFS=$IMAGES_DIR/deploy-ramdisk.initramfs
+ELEMENTS_PATH=/opt/stack/tripleo-image-elements/elements
 
-curl -L -O $DEPLOY_KERNEL_URL
-curl -L -O $DEPLOY_INITRAMFS_URL
-curl -L -O $OVERCLOUD_COMPUTE_URL
-curl -L -O $OVERCLOUD_CONTROL_URL
+export $ELEMENTS_PATH
+
+mkdir -p $IMAGES_DIR
+pushd $IMAGES_DIR
+
+# curl -L -O $DEPLOY_KERNEL_URL
+# curl -L -O $DEPLOY_INITRAMFS_URL
+# curl -L -O $OVERCLOUD_COMPUTE_URL
+# curl -L -O $OVERCLOUD_CONTROL_URL
 
 # This is a fast mirror, for me anyway :-).
-curl -L -O http://mirror.cogentco.com/pub/linux/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
+# curl -L -O http://mirror.cogentco.com/pub/linux/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
 
 popd
+
+if [ ! -f $BM_KERNEL ]; then
+    /opt/stack/diskimage-builder/bin/disk-image-create \
+        -a amd64 \
+        -o $IMAGES_DIR/deploy-ramdisk \
+        fedora deploy
+fi
+
+if [ ! -f $CONTROL_IMG ]; then
+    /opt/stack/diskimage-builder/bin/disk-image-create \
+        -a amd64 \
+        -o $IMAGES_DIR/overcloud-control \
+        fedora boot-stack cinder heat-localip \
+        heat-cfntools neutron-network-node stackuser
+fi
+
+if [ ! -f $COMPUTE_IMG ]; then
+    /opt/stack/diskimage-builder/bin/disk-image-create \
+        -a amd64 \
+        -o $IMAGES_DIR/overcloud-control \
+        fedora overcloud-compute nova-compute nova-kvm \
+        neutron-openvswitch-agent heat-localip heat-cfntools stackuser
+fi
 
 # MACS must be set for setup-baremetal to work
 export MACS=$(bm_poseur get-macs)
