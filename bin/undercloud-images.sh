672d6a131ec7fd06b2d7f3ec488fafa3efcd21ec
James Slagle <jslagle@redhat.com>
2013-08-21 15:02:49 -0400

    Add workaround for upstream nova bug

--- bin/undercloud-images.sh
+++ bin/undercloud-images.sh
@@ -57,5 +57,9 @@ export MACS=$(bm_poseur get-macs)
 # $TRIPLEO_ROOT is not true to the tripleo sense, but it's where
 # setup-baremetal look for the deploy kernel and ramfs.
 TRIPLEO_ROOT=/opt/stack/images /opt/stack/tripleo-incubator/scripts/setup-baremetal 1 1024 10 all
+
+# Workaround for https://bugs.launchpad.net/nova/+bug/1213967
+nova flavor-key baremetal unset cpu_arch
+
 /opt/stack/tripleo-incubator/scripts/load-image $COMPUTE_IMG
 /opt/stack/tripleo-incubator/scripts/load-image $CONTROL_IMG
