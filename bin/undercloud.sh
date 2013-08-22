44ad884750e95a04cb4565ea259035f947875703
James Slagle <jslagle@redhat.com>
2013-08-22 09:33:13 -0400

    use unbuffer for better output

--- bin/undercloud.sh
+++ bin/undercloud.sh
@@ -5,15 +5,18 @@ set -eux
 mkdir -p $HOME/.undercloud-live
 LOG=$HOME/.undercloud-live/undercloud.log
 
-$(dirname $0)/undercloud-install.sh 2>&1 | tee -a $LOG
-$(dirname $0)/undercloud-configure.sh 2>&1 | tee -a $LOG
-$(dirname $0)/undercloud-network.sh 2>&1 | tee -a $LOG
+# This provides the unbuffer command
+sudo yum -y install expect
+
+unbuffer $(dirname $0)/undercloud-install.sh 2>&1 | tee -a $LOG
+unbuffer $(dirname $0)/undercloud-configure.sh 2>&1 | tee -a $LOG
+unbuffer $(dirname $0)/undercloud-network.sh 2>&1 | tee -a $LOG
 
 # starts all services and runs os-refresh-config (via os-collect-config
 # service)
-sudo systemctl daemon-reload 2>&1 | tee -a $LOG
-sudo os-refresh-config 2>&1 | tee -a $LOG
+unbuffer sudo systemctl daemon-reload 2>&1 | tee -a $LOG
+unbuffer sudo os-refresh-config 2>&1 | tee -a $LOG
 
-$(dirname $0)/undercloud-setup.sh 2>&1 | tee -a $LOG
+unbuffer $(dirname $0)/undercloud-setup.sh 2>&1 | tee -a $LOG
 
 echo "undercloud.sh run complete."
