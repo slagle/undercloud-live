c2698a269ea8090c6b50d03759b07f0dded225e7
James Slagle <jslagle@redhat.com>
2013-08-23 15:32:03 -0400

    better output/logging

--- bin/undercloud.sh
+++ bin/undercloud.sh
@@ -5,6 +5,9 @@ set -eux
 mkdir -p $HOME/.undercloud-live
 LOG=$HOME/.undercloud-live/undercloud.log
 
+exec > >(tee $LOG)
+exec 2>&1
+
 PIP_DOWNLOAD_CACHE=${PIP_DOWNLOAD_CACHE:-""}
 
 if [ -z "$PIP_DOWNLOAD_CACHE" ]; then
@@ -17,23 +20,23 @@ fi
 # See: https://bugzilla.redhat.com/show_bug.cgi?id=986667
 sudo mkdir -p /var/lock/subsys
 
-$(dirname $0)/undercloud-install.sh 2>&1 | tee -a $LOG
-$(dirname $0)/undercloud-configure.sh 2>&1 | tee -a $LOG
+$(dirname $0)/undercloud-install.sh
+$(dirname $0)/undercloud-configure.sh
 
 # need to exec to pick up the new group
 if ! id | grep libvirtd; then
     exec sudo su -l $USER $0
 fi
 
-$(dirname $0)/undercloud-network.sh 2>&1 | tee -a $LOG
+$(dirname $0)/undercloud-network.sh
 
 # starts all services and runs os-refresh-config (via os-collect-config
 # service)
-sudo systemctl daemon-reload 2>&1 | tee -a $LOG
-sudo os-refresh-config 2>&1 | tee -a $LOG
+sudo systemctl daemon-reload
+sudo os-refresh-config
 
 # Need to wait for services to finish coming up
 sleep 10
-$(dirname $0)/undercloud-setup.sh 2>&1 | tee -a $LOG
+$(dirname $0)/undercloud-setup.sh
 
 echo "undercloud.sh run complete."
