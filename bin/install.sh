6a5fd91417efca2b504c5fcadd7a1058c67cf809
James Slagle <jslagle@redhat.com>
2013-09-13 15:44:49 -0400

    packaging updates

--- bin/install.sh
+++ bin/install.sh
@@ -13,11 +13,23 @@ sudo yum install -y python-pip
 # busybox is a requirement of ramdisk-image-create from diskimage-builder
 sudo yum install -y busybox
 
+# The packaged version of pbr that gets installed is 
+# python-pbr-0.5.19-2.fc19.noarch
+# However, the unpackaged os-*-config expect pbr>=0.5.21, so we need to still
+# use pip to update pbr for now.
+sudo pip install -U pbr
+
+# This directory is still required because not all the elements in
+# tripleo-puppet-elements has been updated to use packages, specifically
+# os-*-config still use git clones and expect this directory to be created.
 sudo mkdir -m 777 -p /opt/stack
 pushd /opt/stack
 
 git clone https://github.com/slagle/python-dib-elements.git
 git clone https://github.com/slagle/undercloud-live.git
+pushd undercloud-live
+git checkout package
+popd
 
 git clone https://github.com/slagle/tripleo-incubator.git
 pushd tripleo-incubator
@@ -32,6 +44,9 @@ git checkout 9211a7fecbadc13e8254085133df1e3b53f150d8
 popd
 
 git clone https://github.com/agroup/tripleo-puppet-elements
+pushd tripleo-puppet-elements
+git checkout puppet_dev
+popd
 
 git clone https://github.com/openstack/tripleo-heat-templates.git
 pushd tripleo-heat-templates
@@ -58,17 +73,22 @@ if [ ! -e /etc/profile.d/tripleo-incubator-scripts.sh ]; then
     sudo bash -c "echo export PATH=/opt/stack/diskimage-builder/bin/:'\$PATH' >> /etc/profile.d/tripleo-incubator-scripts.sh"
 fi
 
-dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
-    -e fedora \
+# sudo run from nova rootwrap complains about no tty
+sudo sed -i "s/Defaults    requiretty/# Defaults    requiretty/" /etc/sudoers
+# need to be able to pass in a modified $PATH for sudo for dib-elements to work
+sudo sed -i "s/Defaults    secure_path/# Defaults    secure_path/" /etc/sudoers
+
+dib-elements -p diskimage-builder/elements/ tripleo-puppet-elements/elements/ \
+    -e fedora openstack-m-repo \
     -k pre-install \
     -i
-dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
+dib-elements -p diskimage-builder/elements/ tripleo-puppet-elements/elements/ \
     -e source-repositories boot-stack nova-baremetal \
     -k extra-data \
     -i
 # rabbitmq-server does not start with selinux enforcing.
 # https://bugzilla.redhat.com/show_bug.cgi?id=998682
-dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
+dib-elements -p diskimage-builder/elements/ tripleo-puppet-elements/elements/ \
                 undercloud-live/elements \
     -e boot-stack nova-baremetal stackuser heat-cfntools \
        undercloud-live-config selinux-permissive \
@@ -76,9 +96,6 @@ dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
     -i
 
 popd
-
-# sudo run from nova rootwrap complains about no tty
-sudo sed -i "s/Defaults    requiretty/# Defaults    requiretty/" /etc/sudoers
 
 # the current user needs to always connect to the system's libvirt instance
 # when virsh is run
