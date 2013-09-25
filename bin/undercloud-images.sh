4bfd55b07b631a78140663346ffa3332704d33ae
James Slagle <jslagle@redhat.com>
2013-09-25 11:26:10 -0400

    Remove image downloads, they were commented out anyway

--- bin/undercloud-images.sh
+++ bin/undercloud-images.sh
@@ -19,14 +19,6 @@ export ELEMENTS_PATH
 mkdir -p $IMAGES_DIR
 pushd $IMAGES_DIR
 
-# curl -L -O $DEPLOY_KERNEL_URL
-# curl -L -O $DEPLOY_INITRAMFS_URL
-# curl -L -O $OVERCLOUD_COMPUTE_URL
-# curl -L -O $OVERCLOUD_CONTROL_URL
-
-# This is a fast mirror, for me anyway :-).
-# curl -L -O http://mirror.cogentco.com/pub/linux/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
-
 popd
 
 if [ ! -f $BM_KERNEL ]; then
