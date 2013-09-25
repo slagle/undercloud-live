cd5ddd7762b1396ce50c7fab38e4c6aec98061e7
James Slagle <jslagle@redhat.com>
2013-09-25 15:17:23 -0400

    Delete the interface if it already exists so that this script can be re-run without erroring

--- bin/network.sh
+++ bin/network.sh
@@ -13,6 +13,7 @@ PUBLIC_INTERFACE=${PUBLIC_INTERFACE:-ucl0}
 sudo sed -i "s/bridge name='brbm'/bridge name='br-ctlplane'/" /opt/stack/tripleo-incubator/templates/brbm.xml
 /opt/stack/tripleo-incubator/scripts/setup-network
 
+sudo ip link del $PUBLIC_INTERFACE || true
 sudo ip link add $PUBLIC_INTERFACE type dummy
 
 sudo init-neutron-ovs
