a8bbf9e03f1ce235e1c57e099fd46240bbfb6250
James Slagle <jslagle@redhat.com>
2013-09-25 07:17:58 -0400

    Initial script to clean an environment

---
+++ bin/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux
+
+# Stop all services
+pushd /lib/systemd/system
+for s in nova* neutron* glance* keystone* heat* mysql*; do sudo systemctl stop $s; done
+popd
+
+# Clean up directories
+rm -rf /opt/stack
+rm -rf /var/lib/glance
+rm -rf /var/lib/nova
+rm -rf /var/lib/heat
+rm -rf /var/lib/heat-cfntools
+rm -rf /var/lib/neutron
+
+# Clean up libvirt
+for NAME in $(sudo virsh list --name --all | grep "^\(seed\|bootstrap\|baremetal_.*\)$"); do
+    sudo virsh destroy $NAME
+    sudo virsh undefine --remove-all-storage $NAME
+done
+
+for NAME in $(virsh vol-list default | grep /var/ | awk '{print $1}' | grep "^\(seed\|bootstrap\|baremetal-\)" ); do
+    sudo virsh vol-delete --pool default $NAME
+done
