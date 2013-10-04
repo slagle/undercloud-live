7b51e8e2dad6953d9098ad59dbc9a025ffecd5ab
James Slagle <jslagle@redhat.com>
2013-10-04 09:24:04 -0400

    Run os-config-applier earlier in the configure.d phase and move configure and network to that phase

--- bin/install.sh
+++ bin/install.sh
@@ -118,6 +118,9 @@ sudo sed -i "s/eth1/$PUBLIC_INTERFACE/g" /var/lib/heat-cfntools/cfn-init-data
 
 sudo sed -i "s/192.168.122.1/$NETWORK/g" /opt/stack/os-config-applier/templates/var/opt/undercloud-live/masquerade
 
-
+# Need to get a patch upstream for this, but for now, just fix it locally
+# Run os-config-applier earlier in the os-refresh-config configure.d phase
+sudo mv /opt/stack/os-config-refresh/configure.d/50-os-config-applier \
+        /opt/stack/os-config-refresh/configure.d/40-os-config-applier
 
 touch /opt/stack/undercloud-live/.install 
